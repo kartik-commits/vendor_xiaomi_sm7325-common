bc3ba2b42a510b48c9118ceb9f856966313c43f1
Arian <arian.kulmer@web.de>
2024-05-20 18:14:17 +0200

    sm8350-common: Update blobs from V816.0.2.0.UKKMIXM
    Change-Id: I3feeb628c6cfaa8228e3753df0c64af5b9d66b66

--- sm7325-common-vendor.mk
+++ sm7325-common-vendor.mk
@@ -742,8 +742,8 @@ PRODUCT_PACKAGES += \
     IWlanService \
     SoterService \
     TimeService \
-    HotwordEnrollmentOKGoogleHEXAGON \
-    HotwordEnrollmentXGoogleHEXAGON \
+    HotwordEnrollmentOKGoogleHEXAGON_WIDEBAND \
+    HotwordEnrollmentXGoogleHEXAGON_WIDEBAND \
     PowerOffAlarm \
     QtiTelephonyService \
     WfdService \
